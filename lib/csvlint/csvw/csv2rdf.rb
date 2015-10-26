1f7aab2754431f3d216dedb41eb765ef4a8a6be4
Jeni Tennison <jeni@theodi.org>
2015-10-26 13:43:25 +0000

    escape column names correctly (33 failing)

--- lib/csvlint/csvw/csv2rdf.rb
+++ lib/csvlint/csvw/csv2rdf.rb
@@ -217,7 +217,7 @@ module Csvlint
             url = URI.join(@source, url)
           else
             url = column.name || column.default_name || "_col.#{column.number}"
-            url = URI.join(@source, "##{URI.escape(url, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))}")
+            url = URI.join(@source, "##{URI.escape(url, Regexp.new("[^A-Za-z0-9_.]"))}")
           end
           return RDF::Resource.new(url)
         end
